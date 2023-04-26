eb80418c13dd896a3962b117a34b52e01819ae6b
iamogbumitchel <mitchelogbu47@gmail.com>
2023-04-26 09:34:05 +0100

    change

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Footer.css'
 import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+
+    const style = {
+      color: "white",
+      textDecoration: "none",
+    };
   return (
     <div className="footer">
       <div
@@ -30,17 +36,42 @@ const Footer = () => {
           <BsTwitter />
         </div>
       </div>
-      <div className="number">
+      {/* <div className="number">
         <h1>Contact Us</h1>
         <p>+2347031292497</p>
         <p>info@tenhelpinghands.com</p>
+      </div> */}
+      <div
+        className="navigate"
+        style={{
+          backgroundColor: "#353535",
+          color: "white",
+          padding: "15px",
+          width: "250px",
+          borderRadius: "10px",
+        }}
+      >
+        <h1 style={{ color: "#fec006", fontSize: "20px" }}>Navigation</h1>
+        <Link style={style} to="/">
+          <p>Home</p>
+        </Link>
+        <Link style={style} to="/about">
+          <p>About Us</p>
+        </Link>
+        <Link style={style} to="/programs">
+          <p>Programs</p>
+        </Link>
+        <Link style={style} to="/contact">
+          <p>Contact Us</p>
+        </Link>
       </div>
+
       <div
         className="contact-info"
         style={{
           backgroundColor: "#353535",
           color: "white",
-          padding: "35px",
+          padding: "20px",
           width: "400px",
           borderRadius: "10px",
         }}
